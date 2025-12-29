cfa5e5c3a1e4094d94b2faf746940da2abee51aa
ManaSmart <manasmartscent@gmail.com>
2025-12-29 07:48:51 +0200

    invoice addin functionality improvments v1

--- add_updated_by_column.sql
+++
@@ -1,6 +0,0 @@
--- Add missing updated_by column to system_users
-ALTER TABLE system_users 
-ADD COLUMN IF NOT EXISTS updated_by UUID REFERENCES system_users(user_id);
-
--- Then run your fix script again
--- Your fix_login.sql should now work
--- disable_rls_login.sql
+++
@@ -1,5 +0,0 @@
--- Temporarily disable RLS for system_users to allow login
-ALTER TABLE system_users DISABLE ROW LEVEL SECURITY;
-
--- After you can login, we can re-enable with proper policies:
--- ALTER TABLE system_users ENABLE ROW LEVEL SECURITY;
